45948ad04a73b0088f0da73bb8e9e52648d12c9a
carmenelenab <bruma.carmenelena@yahoo.com>
2025-10-03 15:03:40 +0200

    Feat: Implement strict validation in InfoBuilder

--- bp-tracker/src/main/java/com/tracker/bp_tracker/data/PrivateInformations.java
+++ bp-tracker/src/main/java/com/tracker/bp_tracker/data/PrivateInformations.java
@@ -1,9 +1,11 @@
 package com.tracker.bp_tracker.data;
 
 import java.time.LocalDate;
+
 import com.tracker.bp_tracker.details.Gender;
 
 public class PrivateInformations {
+
     private final int id;
     private final String name;
     private final String surname;
@@ -38,35 +40,50 @@ public class PrivateInformations {
         return birthDate;
     }
 
-
     public static class InfoBuilder {
+
         private int id;
         private String name;
         private String surname;
         private Gender gender;
         private LocalDate birthDate;
-    
+
         public InfoBuilder setId(int id) {
             this.id = id;
             return this;
         }
 
         public InfoBuilder setName(String name) {
+            if (name == null || name.isBlank()) {
+                throw new IllegalArgumentException("The name cannot be null or blank!");
+            }
             this.name = name;
             return this;
         }
 
         public InfoBuilder setSurname(String surname) {
+            if (surname == null || surname.isBlank()) {
+                throw new IllegalArgumentException("The surname cannot be null or blank!");
+            }
             this.surname = surname;
             return this;
         }
 
         public InfoBuilder setGender(Gender gender) {
+            if (gender == null) {
+                throw new IllegalArgumentException("The gender cannot be null!");
+            }
             this.gender = gender;
             return this;
         }
 
         public InfoBuilder setBirthDate(LocalDate birthDate) {
+            if (birthDate == null) {
+                throw new IllegalArgumentException("The birth date cannot be null!");
+            }
+            if (birthDate.isAfter(LocalDate.now())) {
+                throw new IllegalArgumentException("The birth date cannot be in the future!");
+            }
             this.birthDate = birthDate;
             return this;
         }
